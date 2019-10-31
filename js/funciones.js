3c48065d1a034ab32141c003d46dba447503b8e3
Diego Martínez Alaminos <55781947+diegomartinezalaminos@users.noreply.github.com>
2019-10-31 21:28:10 +0100

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+function show(element){
+    document.getElementById(element).style.visibility = "visible";
+}
+function sonido(element){
+    document.getElementById(element).play();
+}
+function parar(element){
+    document.getElementById(element).pause();
+}
+function hide(element){
+    document.getElementById(element).style.visibility = "hidden";
+}
